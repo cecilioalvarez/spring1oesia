c79405900499c0b50ff9c88b7cbc333dc125b74c
cecilioAlvarez <cmac@arquitecturajava.com>
2023-10-20 12:33:45 +0200

    nuevos

--- spring1/src/main/java/es/oesia/spring1/WebSecurity.java
+++ spring1/src/main/java/es/oesia/spring1/WebSecurity.java
@@ -2,32 +2,75 @@ package es.oesia.spring1;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurity {
-	
-	  @Bean
-		public BCryptPasswordEncoder passwordEncoder() {
-			return new BCryptPasswordEncoder();
-		}
-	
-	
-	public UserDetailsService userDetailService() {
-		//un sistema de alta de usuarios en memoria
-		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-		manager.createUser(User.withUsername("cecilio")
-				.password(passwordEncoder().encode("cecilio"))
-				.roles("BASICO")
-				.build());
-		
-		return manager;
-		
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
+
+	@Bean
+	public UserDetailsService userDetailService() {
+		// un sistema de alta de usuarios en memoria
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(
+				User.withUsername("cecilio").password(passwordEncoder().encode("cecilio")).roles("BASICO").build());
+
+		return manager;
+
+	}
+
+	//alternativo
+	@Bean
+	public AuthenticationManager authManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder,
+			UserDetailsService userDetailsService) throws Exception {
+		return http.getSharedObject(AuthenticationManagerBuilder.class).userDetailsService(userDetailsService)
+				.passwordEncoder(bCryptPasswordEncoder).and().build();
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager manager) throws Exception {
+
+		http
+		.cors()
+		.and()
+		.csrf()
+		.disable()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.authorizeRequests()
+		.requestMatchers(HttpMethod.POST, "/webapi/login")
+		.permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.addFilterBefore(new FiltroLogin("/webapi/login", manager),
+		UsernamePasswordAuthenticationFilter.class)
+		.addFilterBefore(new FiltroJWTAutenticacion(),
+		UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+
+	}
+	
+	
+	
 
 }
